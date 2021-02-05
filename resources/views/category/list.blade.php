56fbc5ad442c6afa52b28dba98d9bde343d134c7
Maulana Yusril Mahendra <maulanayusrilmahendra@gmail.com>
2021-02-06 00:28:21 +0700

    5.4

--- resources/views/category/list.blade.php
+++ resources/views/category/list.blade.php
@@ -1,2 +1,9 @@
 @extends('master')
 
+@section('content')
+    This is content from category list<br>
+@endsection
+
+{{-- @section('footer')
+    This is footer for category list page
+@endsection --}}
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,16 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Tutorial</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="alert alert-success" role="alert">
-        <h4 class="alert-heading">Well done!</h4>
-        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-        <hr>
-        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-    </div>
+  @include('inc.menu')
+    @yield('content')
+    {{-- @yield('footer') --}}
 </body>
 
 </html>
